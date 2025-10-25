6ead264db2d36f5a551dd4d4c18427b519830e88
jaapeldoorn <verweijw@gmail.com>
2025-10-25 22:06:48 +0200

    script added to retrieve apt status to present this on status page

---
+++ daemon/apt_dump.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sudo aptitude update
+sudo aptitude -F%p --disable-columns search ~U > /etc/LinuxMonitor/web/apt/RaspiServ3_upgrade.txt
+sudo aptitude -F%p --disable-columns search ~o > /etc/LinuxMonitor/web/apt/RaspiServ3_autoremove.txt
+
