4f7b44f8bd8459c177f9e436b017c70215138f84
jaapeldoorn <verweijw@gmail.com>
2025-11-06 19:41:52 +0100

    Reduced output from apt update

--- daemon/apt_dump.sh
+++ daemon/apt_dump.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo aptitude update
+sudo aptitude update > /dev/null
 sudo aptitude -F%p --disable-columns search ~U > /etc/LinuxMonitor/web/apt/RaspiServ3_upgrade.txt
 sudo aptitude -F%p --disable-columns search ~o > /etc/LinuxMonitor/web/apt/RaspiServ3_autoremove.txt
 
